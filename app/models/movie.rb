75d37dea92cf85745258b8a6b0f0f0282328a0ec
Alec Lorraine <alec@brighten.in>
2017-09-07 16:46:11 -0700

    image magick

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
 
-  has_attached_file :image
+  has_attached_file :image, styles: {
+    small: "90x133>",
+    thumb: "50x50>"
+  }
 
   has_many :reviews, dependent: :destroy
 
